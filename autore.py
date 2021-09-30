1bcf24c5849dba3f8da25e3d42728e2bf95473d7
heyxferdi <87966788+heyxferdi@users.noreply.github.com>
2021-09-30 23:17:25 +0700

    updates on autore.py
    some updates to make this script working.

--- autore.py
+++ autore.py
@@ -6,7 +6,7 @@ from telethon import events
 api_id = 'put your api_id here'
 api_hash = 'put yout api_hash here'
 
-client = TelegramClient('Username', api_id, api_hash)
+client = TelegramClient('username', api_id, api_hash)
 
 message = 'Hey, I will be at a retreat until Saturday. I promise to get in touch as soon as I can. See you.'
 
@@ -18,7 +18,7 @@ def main():
     async def _(event):
         if event.is_private:
             time.sleep(1)  # pause for 1 second to rate-limit automatic replies
-            await client.send_message(event.message.from_id, message)
+            await event.respond(message)
     client.run_until_disconnected()
 
 
